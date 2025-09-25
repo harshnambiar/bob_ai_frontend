3e69ead9d114a3b5741d46d55c23caa4eaf4f9b5
harshnambiar <harrisonlvl2@gmail.com>
2025-09-25 15:48:25 +0530

    added more queries

--- src/index.js
+++ src/index.js
@@ -119,7 +119,7 @@ window.disconnectIfNecessary = disconnectIfNecessary
 
 
 async function form1(){
-    window.open("https://form.jotform.com/252172180330041", "_blank");
+    window.open("https://form.jotform.com/252672546274059", "_blank");
 }
 window.form1 = form1;
 
